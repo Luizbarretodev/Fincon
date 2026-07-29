ea9bdd2777b4e0f7a04415deed19ea256f3e744e
Luizbarretodev <gugajoao1020@gmail.com>
2026-07-28 22:53:58 -0300

    feat: adiciona entidades categoriaEntrada e categoriaSaida

---
+++ Fincon.Domain/Entities/CategoriaEntrada.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Domain.Entities;
+
+public class CategoriaEntrada
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+
+    public CategoriaEntrada(string nome)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            throw new ArgumentException("Nome é obrigatório", nameof(nome));
+        }
+        Nome = nome;
+
+        Id = Guid.NewGuid();
+    }
+}
