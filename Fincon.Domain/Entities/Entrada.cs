74c32f81ec9043a3a21db45b0d8e31b2d0044ebf
Luizbarretodev <gugajoao1020@gmail.com>
2026-07-29 21:18:14 -0300

    feat: adiciona Entrada às entidades de Domain

---
+++ Fincon.Domain/Entities/Entrada.cs
@@ -0,0 +1,24 @@
+﻿using Fincon.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Domain.Entities;
+
+public class Entrada : Transacao
+{
+    public Guid CategoriaEntradaId { get; private set; }
+
+    public Entrada(DateTime data, decimal valor, string descricao, StatusTransacao status, Guid contaId, Guid? recorrenciaId, Guid categoriaEntradaId)
+        : base(data, valor, descricao, status, contaId, recorrenciaId)
+    {
+        if(categoriaEntradaId == Guid.Empty)
+        {
+            throw new ArgumentException("Id não pode ser vazio", nameof(contaId));
+        }
+
+        CategoriaEntradaId = categoriaEntradaId;
+    }
+}
