751d8354db30b522d075c61f39c57b1c534dfe5d
Luizbarretodev <gugajoao1020@gmail.com>
2026-07-30 22:04:08 -0300

    feat: adiciona classe context e criação das tabelas

---
+++ Fincon.Infraestucture/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Fincon.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Infrastucture.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Conta> Contas { get; set; }
+    public DbSet<CategoriaEntrada> CategoriasEntrada { get; set; }
+    public DbSet<CategoriaSaida> CategoriasSaida { get; set; }
+    public DbSet<Entrada> Entradas { get; set; }
+    public DbSet<Saida> Saidas { get; set; }
+    public DbSet<Recorrencia> Recorrencias { get; set; }
+
+}
