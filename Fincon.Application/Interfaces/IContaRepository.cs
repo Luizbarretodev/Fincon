d29b0138b051aeb7fb6a0ceb39d4129eb68cd0ff
Luizbarretodev <gugajoao1020@gmail.com>
2026-07-31 22:50:18 -0300

    feat: adiciona lógica de criação de conta ao application

--- Fincon.Application/Interfaces/IContaRepository.cs
+++ Fincon.Application/Interfaces/IContaRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fincon.Application;
+namespace Fincon.Application.Interfaces;
 
 public interface IContaRepository
 {
---
+++ Fincon.Application/UseCases/Contas/CriaContaUseCase.cs
@@ -0,0 +1,27 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Contas;
+
+public class CriaContaUseCase 
+{
+    private readonly IContaRepository _contaRepository;
+
+    public CriaContaUseCase(IContaRepository contaRepository)
+    {
+        _contaRepository = contaRepository;
+    }
+
+    public async Task<Conta> ExecutarAsync(string nome)
+    {
+        var conta = new Conta(nome);
+        await _contaRepository.CriarContaAsync(conta);
+
+        return conta;
+    }
+}
