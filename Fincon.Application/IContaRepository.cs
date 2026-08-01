5d44894f9a0b9f813a8a19d457d0987cf9b891aa
Luizbarretodev <gugajoao1020@gmail.com>
2026-07-31 22:31:29 -0300

    feat: Adiciona interface IContaRepository

---
+++ Fincon.Application/IContaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fincon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application;
+
+public interface IContaRepository
+{
+    Task CriarContaAsync(Conta conta);
+}
