e52b45eef709cb895b3f997ce9ab778437900cf1
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-01 21:48:24 -0300

    feat: adiciona controllers de entrada e saida de categorias

---
+++ Fincon.Api/Controllers/CategoriasEntradaController.cs
@@ -0,0 +1,25 @@
+﻿using Fincon.Api.Models;
+using Fincon.Application.UseCases.Categorias;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fincon.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriasEntradaController : ControllerBase
+{
+    private readonly CriaEntradaUseCase _criaEntradaUseCase;
+
+    public CategoriasEntradaController(CriaEntradaUseCase criaEntradaUseCase)
+    {
+        _criaEntradaUseCase = criaEntradaUseCase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CriaCategoriaEntradaRequest request)
+    {
+        var entrada = await _criaEntradaUseCase.ExecutarAsync(request.Nome);
+
+        return Ok(new { entrada.Id, entrada.Nome });
+    }
+}
---
+++ Fincon.Api/Controllers/CategoriasSaidaController.cs
@@ -0,0 +1,25 @@
+﻿using Fincon.Api.Models;
+using Fincon.Application.UseCases.Categorias;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fincon.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriasSaidaController : ControllerBase
+{
+    private readonly CriaSaidaUseCase _criaSaidaUseCase;
+
+    public CategoriasSaidaController(CriaSaidaUseCase criaSaidaUseCase)
+    {
+        _criaSaidaUseCase = criaSaidaUseCase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CriaCategoriaSaidaRequest request)
+    {
+        var saida = await _criaSaidaUseCase.ExecutarAsync(request.Nome);
+
+        return Ok(new { saida.Id, saida.Nome });
+    }
+}
