77a68ad460288c0872c03781c2c8c8decbbe04e5
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-01 21:21:52 -0300

    feat: adiciona injeção de dependencia de entrada e saida

--- Fincon.Api/Program.cs
+++ Fincon.Api/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IContaRepository, ContaRepository>();
+builder.Services.AddScoped<ICategoriaEntradaRepository, CategoriaEntradaRepository>();
+builder.Services.AddScoped<ICategoriaSaidaRepository, CategoriaSaidaRepository>();
 builder.Services.AddScoped<CriaContaUseCase>();
 
 
