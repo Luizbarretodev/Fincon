e3ea0d46349e410f36c538b32982765463370def
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-03 20:46:51 -0300

    feat: cria recorrencia use case

---
+++ Fincon.Application/UseCases/Recorrencias/CriarRecorrenciaUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Domain.Entities;
+using Fincon.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Recorrencias;
+
+public class CriarRecorrenciaUseCase
+{
+    private readonly IRecorrenciaRepository _recorrenciaRepository;
+
+    public CriarRecorrenciaUseCase(IRecorrenciaRepository recorrenciaRepository)
+    {
+        recorrenciaRepository = recorrenciaRepository;
+    }
+
+    public async Task<Recorrencia> ExecutarAsync(string descricao, decimal valorParcela, int quantidadeParcelas, 
+                                                 DateTime dataInicio, TipoRecorrencia tipo)
+    {
+        var recorrencia = new Recorrencia(descricao, valorParcela, quantidadeParcelas, dataInicio, tipo);
+
+        await _recorrenciaRepository.AdicionarAsync(recorrencia);
+        return recorrencia;
+    }
+}
