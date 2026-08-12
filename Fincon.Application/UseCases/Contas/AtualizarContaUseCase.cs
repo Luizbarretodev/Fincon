ee0e5f1805120df97fd46cc52d30ff90f8ff2767
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-11 21:36:15 -0300

    feat: adiciona use cases pra atualizar e excluir conta

---
+++ Fincon.Application/UseCases/Contas/AtualizarContaUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Contas;
+
+public class AtualizarContaUseCase
+{
+    private readonly IContaRepository _contaRepository;
+
+    public AtualizarContaUseCase(IContaRepository contaRepository)
+    {
+        _contaRepository = contaRepository;
+    }
+
+    public async Task<Conta> ExecutarAsync(Guid id, string nome)
+    {
+        var conta = await _contaRepository.ObterPorIdAsync(id);
+        if (conta == null)
+            throw new ArgumentException("Conta não encontrada", nameof(id));
+
+        conta.AtualizarNome(nome);
+        await _contaRepository.AtualizarAsync(conta);
+        return conta;
+    }
+}
---
+++ Fincon.Application/UseCases/Contas/ExcluirContaUseCase.cs
@@ -0,0 +1,23 @@
+﻿using Fincon.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Contas;
+
+public class ExcluirContaUseCase
+{
+    private readonly IContaRepository _contaRepository;
+
+    public ExcluirContaUseCase(IContaRepository contaRepository)
+    {
+        _contaRepository = contaRepository;
+    }
+
+    public async Task ExecutarAsync(Guid id)
+    {
+        await _contaRepository.ExcluirAsync(id);
+    }
+}
