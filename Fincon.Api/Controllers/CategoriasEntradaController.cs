00734a023434fa3f04132f830303a2a2c0dc8ecd
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-12 23:09:25 -0300

    feat: adiciona endpoint de atualizar categoria entrada

--- Fincon.Api/Controllers/CategoriasEntradaController.cs
+++ Fincon.Api/Controllers/CategoriasEntradaController.cs
@@ -10,14 +10,11 @@ public class CategoriasEntradaController : ControllerBase
 {
     private readonly CriarCategoriaEntradaUseCase _criaCategoriaEntradaUseCase;
     private readonly ListarCategoriasEntradaUseCase _listaCategoriasEntradaUseCase;
-    private readonly AtualizarCategoriaEntradaUseCase _atualizarCategoriaEntradaUseCase;
 
-    public CategoriasEntradaController(CriarCategoriaEntradaUseCase criaEntradaUseCase, ListarCategoriasEntradaUseCase listaCategoriasEntradaUseCase,
-                                       AtualizarCategoriaEntradaUseCase atualizarCategoriaEntradaUseCase)
+    public CategoriasEntradaController(CriarCategoriaEntradaUseCase criaEntradaUseCase, ListarCategoriasEntradaUseCase listaCategoriasEntradaUseCase)
     {
         _criaCategoriaEntradaUseCase = criaEntradaUseCase;
         _listaCategoriasEntradaUseCase = listaCategoriasEntradaUseCase;
-        _atualizarCategoriaEntradaUseCase = atualizarCategoriaEntradaUseCase;
     }
 
     [HttpGet]
@@ -34,19 +31,5 @@ public class CategoriasEntradaController : ControllerBase
         var entrada = await _criaCategoriaEntradaUseCase.ExecutarAsync(request.Nome);
 
         return Ok(new { entrada.Id, entrada.Nome });
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(Guid id, [FromBody] AtualizarCategoriaEntradaRequest request)
-    {
-        try
-        {
-            var entrada = await _atualizarCategoriaEntradaUseCase.ExecutarAsync(id, request.nome);
-            return Ok(new { entrada.Id, entrada.Nome });
-        }
-        catch(ArgumentException ex)
-        {
-            return BadRequest(ex.Message);
-        }
     }
 }
--- Fincon.Api/Models/AtualizarCategoriaEntradaRequest.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Fincon.Api.Models;
-
-public record AtualizarCategoriaEntradaRequest(string nome);
