12448f9290a4297a73caaea4e0b99de08ffe809f
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-13 22:34:03 -0300

    feat: adiciona metodos de atualizar e excluir categoria saida

---
+++ Fincon.Application/UseCases/Categorias/AtualizarCategoriaSaidaUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Categorias;
+
+public class AtualizarCategoriaSaidaUseCase
+{
+    private readonly ICategoriaSaidaRepository _categoriaSaidaRepository;
+    public AtualizarCategoriaSaidaUseCase(ICategoriaSaidaRepository categoriaSaidaRepository)
+    {
+        _categoriaSaidaRepository = categoriaSaidaRepository;
+    }
+
+    public async Task<CategoriaSaida> ExecutarAsync(Guid id, string nome)
+    {
+        var categoriaSaida = await _categoriaSaidaRepository.ObterPorIdAsync(id);
+        if (categoriaSaida == null)
+            throw new ArgumentException("Categoria não encontrada", nameof(id));
+
+        categoriaSaida.AtualizarNome(nome);
+        await _categoriaSaidaRepository.AtualizarAsync(categoriaSaida);
+        return categoriaSaida;
+    }
+}
---
+++ Fincon.Application/UseCases/Categorias/ExcluirCategoriaSaidaAsync.cs
@@ -0,0 +1,22 @@
+﻿using Fincon.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Categorias;
+
+public class ExcluirCategoriaSaidaUseCase
+{
+    private readonly ICategoriaSaidaRepository _categoriaSaidaRepository;
+    public ExcluirCategoriaSaidaUseCase(ICategoriaSaidaRepository categoriaSaidaRepository)
+    {
+        _categoriaSaidaRepository = categoriaSaidaRepository;
+    }
+
+    public async Task ExecutarAsync(Guid id)
+    {
+        await _categoriaSaidaRepository.ExcluirAsync(id);
+    }
+}
