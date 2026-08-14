cc8e389e0458433e9f1aaab0b21aa1f98cb9b650
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-13 22:36:37 -0300

    feat: adiciona DTO e endpoints de atualizar e excluir categoria saida

--- Fincon.Api/Controllers/CategoriasSaidaController.cs
+++ Fincon.Api/Controllers/CategoriasSaidaController.cs
@@ -10,6 +10,8 @@ public class CategoriasSaidaController : ControllerBase
 {
     private readonly CriarCategoriaSaidaUseCase _criaCategoriaSaidaUseCase;
     private readonly ListarCategoriasSaidaUseCase _listaCategoriasSaidaUseCase;
+    private readonly AtualizarCategoriaSaidaUseCase _atualizarCategoriaSaidaUseCase;
+    private readonly ExcluirCategoriaSaidaUseCase _excluirCategoriaSaidaUseCase;
 
     public CategoriasSaidaController(CriarCategoriaSaidaUseCase criaSaidaUseCase, ListarCategoriasSaidaUseCase listaCategoriasSaidaUseCase)
     {
@@ -31,5 +33,26 @@ public class CategoriasSaidaController : ControllerBase
         var saida = await _criaCategoriaSaidaUseCase.ExecutarAsync(request.Nome);
 
         return Ok(new { saida.Id, saida.Nome });
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(Guid id, [FromBody] AtualizarCategoriaSaidaRequest request)
+    {
+        try
+        {
+            var categoriaSaida = await _atualizarCategoriaSaidaUseCase.ExecutarAsync(id, request.Nome);
+            return Ok(new { categoriaSaida.Id, categoriaSaida.Nome });
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _excluirCategoriaSaidaUseCase.ExecutarAsync(id);
+        return NoContent();
     }
 }
---
+++ Fincon.Api/Models/AtualizarCategoriaSaidaRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Fincon.Api.Models;
+
+public record AtualizarCategoriaSaidaRequest(string Nome);
