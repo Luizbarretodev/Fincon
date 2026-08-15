a747f12bdede250fdfeeaa89ca69fb231f1089e0
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-14 23:35:10 -0300

    feat: adiciona use cases de atualizar e excluir cat. entrada

---
+++ Fincon.Application/UseCases/Categorias/AtualizarCategoriaEntradaUseCase.cs
@@ -0,0 +1,22 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Domain.Entities;
+
+public class AtualizarCategoriaEntradaUseCase
+{
+    private readonly ICategoriaEntradaRepository _categoriaEntradaRepository;
+    public AtualizarCategoriaEntradaUseCase(ICategoriaEntradaRepository categoriaEntradaRepository)
+    {
+        _categoriaEntradaRepository = categoriaEntradaRepository;
+    }
+
+    public async Task<CategoriaEntrada> ExecutarAsync(Guid id, string nome)
+    {
+        var categoriaEntrada = await _categoriaEntradaRepository.ObterPorIdAsync(id);
+        if (categoriaEntrada == null)
+            throw new ArgumentException("Categoria não encontrada", nameof(id));
+
+        categoriaEntrada.AtualizarNome(nome);
+        await _categoriaEntradaRepository.AtualizarAsync(categoriaEntrada);
+        return categoriaEntrada;
+    }
+}
---
+++ Fincon.Application/UseCases/Categorias/ExcluirCategoriaEntradaUseCase.cs
@@ -0,0 +1,13 @@
+﻿using Fincon.Application.Interfaces;
+
+public class ExcluirCategoriaEntradaUseCase
+{
+    private readonly ICategoriaEntradaRepository _categoriaEntradaRepository;
+    public ExcluirCategoriaEntradaUseCase(ICategoriaEntradaRepository categoriaEntradaRepository)
+        => _categoriaEntradaRepository = categoriaEntradaRepository;
+
+    public async Task ExecutarAsync(Guid id)
+    {
+        await _categoriaEntradaRepository.ExcluirAsync(id);
+    }
+}
