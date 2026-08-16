63dd4685b4b8c1b56fcb0b481d6f1065675b4a40
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-15 23:38:10 -0300

    feat: adiciona excluir entrada no use case

---
+++ Fincon.Application/UseCases/Movimentacoes/ExcluirEntradaUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Fincon.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Movimentacoes;
+
+public class ExcluirEntradaUseCase
+{
+    private readonly IEntradaRepository _entradaRepository;
+    public ExcluirEntradaUseCase(IEntradaRepository entradaRepository) => _entradaRepository = entradaRepository;
+
+    public async Task ExecutarAsync(Guid id)
+    {
+        await _entradaRepository.ExcluirAsync(id);
+    }
+}
