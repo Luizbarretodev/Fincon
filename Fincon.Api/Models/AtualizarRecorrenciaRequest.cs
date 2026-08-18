50e27a5d265b670e2f7b1e01fff80257705cb9ef
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-17 21:43:25 -0300

    feat: adiciona DTO para atualizar Recorrencia

---
+++ Fincon.Api/Models/AtualizarRecorrenciaRequest.cs
@@ -0,0 +1,11 @@
+﻿using Fincon.Domain.Enums;
+
+namespace Fincon.Api.Models;
+
+public record AtualizarRecorrenciaRequest(
+ string Descricao,
+ decimal ValorParcela,
+ int QuantidadeParcelas,
+ DateTime DataInicio,
+ TipoRecorrencia Tipo
+);
