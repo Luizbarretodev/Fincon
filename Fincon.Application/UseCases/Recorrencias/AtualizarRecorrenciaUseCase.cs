e859fc79c90fb383c5cc562f1cd30efd064c1423
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-17 21:40:50 -0300

    feat: adiciona use case p/ atualizar recorrencia

---
+++ Fincon.Application/UseCases/Recorrencias/AtualizarRecorrenciaUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Domain.Entities;
+using Fincon.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Recorrencias;
+
+public class AtualizarRecorrenciaUseCase
+{
+    private readonly IRecorrenciaRepository _recorrenciaRepository;
+    public AtualizarRecorrenciaUseCase(IRecorrenciaRepository recorrenciaRepository)
+    {
+        _recorrenciaRepository = recorrenciaRepository;
+    }
+
+    public async Task<Recorrencia> ExecutarAsync(Guid id, string descricao, decimal valorParcela, int quantidadeParcelas, DateTime dataInicio, TipoRecorrencia tipo)
+    {
+        var recorrencia = await _recorrenciaRepository.ObterPorIdAsync(id);
+        if (recorrencia == null)
+            throw new ArgumentException("Recorrência não encontrada", nameof(id));
+
+        recorrencia.Atualizar(descricao, valorParcela, quantidadeParcelas, dataInicio, tipo);
+        await _recorrenciaRepository.AtualizarAsync(recorrencia);
+        return recorrencia;
+    }
+}
