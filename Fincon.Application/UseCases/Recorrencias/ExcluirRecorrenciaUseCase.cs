852c2785db9f795a3a1e52f170ceca472c33f9b4
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-17 21:41:53 -0300

    feat: adiciona use case p/ excluir recorrencia

---
+++ Fincon.Application/UseCases/Recorrencias/ExcluirRecorrenciaUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Fincon.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Recorrencias;
+
+public class ExcluirRecorrenciaUseCase
+{
+    private readonly IRecorrenciaRepository _recorrenciaRepository;
+    public ExcluirRecorrenciaUseCase(IRecorrenciaRepository recorrenciaRepository)
+        => _recorrenciaRepository = recorrenciaRepository;
+
+    public async Task ExecutarAsync(Guid id)
+    {
+        await _recorrenciaRepository.ExcluirAsync(id);
+    }
+}
