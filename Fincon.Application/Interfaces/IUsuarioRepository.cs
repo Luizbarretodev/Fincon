797f8061efcb61f306678f3cbc8ecad28c13181e
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-01 22:47:57 -0300

    feat: add interface de contratos de usuario

---
+++ Fincon.Application/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using Fincon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.Interfaces;
+
+public interface IUsuarioRepository
+{
+    Task AdicionarAsync(Usuario usuario);
+    Task<Usuario?> ObterPorEmailAsync(string email);
+    Task<bool> EmailExisteAsync(string email);
+}
