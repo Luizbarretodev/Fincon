17e4a8fd3b31eb4f9c9dbba26aee3f2d9bf2b1c4
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-01 23:00:25 -0300

    feat: implementa contrato de usuario repository

---
+++ Fincon.Infraestucture/Repositories/UsuarioRepository.cs
@@ -0,0 +1,36 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Domain.Entities;
+using Fincon.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Infrastructure.Repositories;
+
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly AppDbContext _context;
+
+    public UsuarioRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Usuario?> ObterPorEmailAsync(string email)
+    {
+        return await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<bool> EmailExisteAsync(string email)
+    {
+        return await _context.Usuarios.AnyAsync(u => u.Email == email);
+    }
+
+    public async Task AdicionarAsync(Usuario usuario)
+    {
+        _context.Usuarios.Add(usuario);
+        await _context.SaveChangesAsync();
+    }
+}
