2b4108ada85791233a1f491e5de7a480ea7ae9d7
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-02 22:02:09 -0300

    feat: add contrato de hash de senha e verificação do mesmo

---
+++ Fincon.Application/Interfaces/Auth/ISenhaHasher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.Interfaces.Auth
+{
+    internal class ISenhaHasher
+    {
+    }
+}
