2edcad024b75b42719e69b40784b1dd5c0706c81
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-17 23:01:48 -0300

    feat: add método de LoginUseCase

---
+++ Fincon.Application/UseCases/Auth/LoginUseCase.cs
@@ -0,0 +1,44 @@
+﻿using Fincon.Application.Interfaces;
+using Fincon.Application.Interfaces.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincon.Application.UseCases.Auth;
+
+public class LoginUseCase
+{
+    private readonly IUsuarioRepository _usuarioRepository;
+    private readonly ISenhaHasher _senhaHasher;
+    private readonly IJwtGenerator _jwtGenerator;
+
+    public LoginUseCase(IUsuarioRepository usuarioRepository, ISenhaHasher senhaHasher, IJwtGenerator jwtGenerator)
+    {
+        _usuarioRepository = usuarioRepository;
+        _senhaHasher = senhaHasher;
+        _jwtGenerator = jwtGenerator;
+    }
+
+    public async Task<string> ExecutarAsync(string email, string senha)
+    {
+        var usuario = await _usuarioRepository.ObterPorEmailAsync(email);
+
+        if (usuario is null)
+        {
+            throw new ArgumentException("Email ou senha inválidos", nameof(email));
+        }
+
+        var verificaSenha = _senhaHasher.VerificarHash(usuario.SenhaHash, senha);
+
+        if (!verificaSenha)
+        {
+            throw new ArgumentException("Email ou senha inválidos", nameof(email));
+        }
+
+        var token = _jwtGenerator.GerarToken(usuario);
+
+        return token;
+    }
+}
